206761f3f792b01d11718a6166f49e6d777bc00e
ProkosaOstap <prokosaon@gmail.com>
2023-10-08 21:53:10 +0300

    lab2.1

---
+++ lolkek2.1.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int main ()
+{
+    double x, z1, z2;
+    printf("Enter the x: ");
+    scanf("%2Lf", &x);
+    z1 = 1 - pow(sin(2*x), 2) / 4 + cos(2*x);
+    z2 = pow(cos(x), 2) + pow(cos(x),4);
+
+    printf("z1 = %2Lf, z2 = %2Lf", z1, z2);
+
+    getch();
+
+
+}
